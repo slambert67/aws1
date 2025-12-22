67afe055f21fc6de68892cf0e8dfc3adcadf9117
Steve Lambert <steve.lambert67@gmail.com>
2025-12-22 11:54:21 +0000

    New image pushed from github and also deployed for the second time!

--- my-first-aws-app/apps/aws/src/app.service.ts
+++ my-first-aws-app/apps/aws/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'New image pushed from github and also deployed!';
+    return 'New image pushed from github and also deployed for the second time!';
   }
 }
